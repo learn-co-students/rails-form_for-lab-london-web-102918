254c28e6b99294614b660745b1401c2a4b8d4420
github username <github email address>
2018-11-26 13:37:09 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,39 +1,33 @@
 class SchoolClassesController < ApplicationController
-    def index
-      @school_classes = SchoolClass.all
+
+  def index
+		@school_classes = SchoolClass.all
+	end
+
+	def show
+		@school_class = SchoolClass.find(params[:id])
+	end
+
+	def new
+		@school_class = SchoolClass.new
+	end
+
+	def create
+	  @school_class = SchoolClass.new(params.require(:school_class))
+	  # @school_class.title = params[:title]
+	  # @school_class.room_number = params[:room_number]
+	  @school_class.save
+	  redirect_to school_class_path(@school_class)
+	end
+
+	def edit
+		@school_class = SchoolClass.find(params[:id])
+	end
+
+	def update
+	  @school_class = SchoolClass.find(params[:id])
+	  @school_class.update(params.require(:school_class))
+	  redirect_to school_class_path(@school_class)
     end
-  
-    def show
-      the_class
-    end
-  
-    def new
-      @school_class = SchoolClass.new
-    end
-  
-    def create
-      @school_class = SchoolClass.new(param(:title, :room_number))
-      @school_class.save
-      redirect_to school_class_path(@school_class)
-    end
-  
-    def edit
-      the_class
-    end
-  
-    def update
-      the_class
-      the_class.update(param(:title, :room_number))
-      redirect_to school_class_path(the_class)
-    end
-  
-    private
-  
-    def the_class
-      @school_class = SchoolClass.find(params[:id])
-    end
-  
-    def param(*arg)
-      params.require(:school_class).permit(*arg)
-    end
-  end
+
+end
